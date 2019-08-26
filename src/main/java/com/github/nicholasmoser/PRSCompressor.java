ee458fb8a7582ffe606bd2704e423a232918096b
Nick <NicholasMoser56@gmail.com>
2019-08-25 20:42:48 -0500

    PRSCompressor refactors

--- src/main/java/com/github/nicholasmoser/PRSCompressor.java
+++ src/main/java/com/github/nicholasmoser/PRSCompressor.java
@@ -3,13 +3,19 @@ package com.github.nicholasmoser;
 import java.util.Arrays;
 
 /**
- * A compressor for Eighting PRS compressed files. Takes in the full byte array of the file and returns the uncompressed byte stream. Thanks to Luigi Auriemma for porting to QuickBMS. Thanks to
- * RupertAvery for originally writing it: https://gbatemp.net/threads/tvc-fpk-tool.207232/ http://www.mediafire.com/?vty5jymlmm2
+ * A compressor for Eighting PRS compressed files. Takes in the full byte array of the file and returns the uncompressed byte stream.
+ * Thanks to Luigi Auriemma for porting to QuickBMS. Thanks to RupertAvery for originally writing it:
+ * 
+ * https://gbatemp.net/threads/tvc-fpk-tool.207232/
+ * http://www.mediafire.com/?vty5jymlmm2
  */
 public class PRSCompressor
 {
     // Input bytes to compress.
     private byte[] input;
+    
+    // Output bytes that have been compressed.
+    private byte[] output;
 
     // The location in the output bytes of the current flag byte.
     private int flagIndex;
@@ -23,7 +29,8 @@ public class PRSCompressor
     // The current bit index inside of the flag byte.
     private int flagBitIndex;
 
-    private int length;
+    // The length of the input bytes currently being compressed.
+    private int currentCompressionLength;
 
     private int pos;
 
@@ -38,9 +45,10 @@ public class PRSCompressor
         flagIndex = 0;
         outputIndex = 1;
         flagBitIndex = 7;
-        length = 0;
+        currentCompressionLength = 0;
         pos = 0;
         inputIndex = 0;
+        output = new byte[input.length * 2];
     }
 
     /**
@@ -50,80 +58,75 @@ public class PRSCompressor
      */
     public byte[] compress()
     {
-        byte[] output = new byte[input.length * 2];
-
         while (inputIndex < input.length)
         {
-            if (check_window(inputIndex))
+            if (check_window())
             {
-                writeCompressedBytes(output, length, pos);
-                inputIndex += length;
+                writeCompressedBytes();
+                inputIndex += currentCompressionLength;
             }
             else if (checkRunLengthEncoding())
             {
-                writeCompressedBytes(output, length, pos);
-                inputIndex += length;
+                writeCompressedBytes();
+                inputIndex += currentCompressionLength;
             }
             else
             {
-                writeUncompressedByte(output);
+                writeUncompressedByte();
                 inputIndex++;
             }
         }
-        terminateFile(output);
+        terminateFile();
 
-        // Return the length of the output plus three so that the file ends with three
-        // 0s.
+        // Return the length of the output plus three so that the file ends with three 0s.
         return Arrays.copyOfRange(output, 0, (outputIndex) + 3);
     }
 
     /**
      * Terminates the file by adding a 0 and 1 bit to the flag. This is equivalent to a long search, but yields no result.
-     * 
-     * @param output The output bytes.
      */
-    private void terminateFile(byte[] output)
+    private void terminateFile()
     {
-        writeBit(output, 0);
-        writeBit(output, 1);
+        writeBit(0);
+        writeBit(1);
     }
 
     /**
-     * Tests the current buffer position for run length encoding (RLE). This will scan ahead of the current position until the byte changes or the maximum of 256 is reached.
+     * Tests the current buffer position for run length encoding (RLE). This will scan ahead of the current position until the byte
+     * changes or the maximum of 256 is reached.
      * 
      * @return If run length encoding is found.
      */
     private boolean checkRunLengthEncoding()
     {
         int p = inputIndex;
-        length = 0;
+        currentCompressionLength = 0;
         pos = 1;
 
         if (inputIndex < pos)
         {
             return false;
         }
-        while (p < input.length && input[inputIndex - pos] == input[p] && length < 256)
+        while (p < input.length && input[inputIndex - pos] == input[p] && currentCompressionLength < 256)
         {
-            length++;
+            currentCompressionLength++;
             p++;
         }
 
-        return length > 1;
+        return currentCompressionLength > 1;
     }
 
     /**
      * Scans backwards from the current buffer position
      * 
-     * @param newsptr
      * @return If previous repeated occurrences were found.
      */
-    private boolean check_window(int newsptr)
+    private boolean check_window()
     {
         int tlength = 1;
         int tpos = -1;
 
-        if (newsptr < tlength)
+        if (inputIndex < tlength)
         {
             return false;
         }
@@ -135,8 +138,8 @@ public class PRSCompressor
             tlengthMax = bytesLeft + 1;
         }
 
-        int p = newsptr - tlength;
-        int curr = newsptr;
+        int p = inputIndex - tlength;
+        int curr = inputIndex;
         int savep = p;
 
         // scan area = 8192 bytes
@@ -155,7 +158,7 @@ public class PRSCompressor
         tlength--;
         tpos = curr - savep;
 
-        length = tlength;
+        currentCompressionLength = tlength;
         pos = tpos;
 
         if ((tlength == 2) && (tpos > 255))
@@ -184,10 +187,9 @@ public class PRSCompressor
     /**
      * Writes a bit to the current flag byte. If there are no more bits left in the current flag byte, creates a new flag byte at the current output index and writes to it.
      * 
-     * @param output The output bytes.
      * @param bit The bit to write.
      */
-    private void writeBit(byte[] output, int bit)
+    private void writeBit(int bit)
     {
         if (flagBitIndex == -1)
         {
@@ -200,22 +202,22 @@ public class PRSCompressor
     }
 
     /**
-     * Compresses between 2 and 5 bytes reachable through a short search. This includes the bits 0 and 0 in the flag byte, which indicates that this byte is compressed and is short.
+     * Compresses between 2 and 5 bytes reachable through a short search. This includes the bits 0 and 0 in the flag byte,
+     * which indicates that this byte is compressed and is short.
      * 
-     * @param output The output bytes.
      * @param len
      * @param posy
      */
-    private void writeBytesShortCompression(byte[] output, int len, int posy)
+    private void writeBytesShortCompression(int len, int posy)
     {
         int ctr = 2;
-        writeBit(output, 0);
-        writeBit(output, 0);
+        writeBit(0);
+        writeBit(0);
 
         len -= 2;
         while (ctr > 0)
         {
-            writeBit(output, (len >> 1) & 0x01);
+            writeBit((len >> 1) & 0x01);
             len = (len << 1) & 0x02;
             ctr--;
         }
@@ -224,16 +226,16 @@ public class PRSCompressor
     }
 
     /**
-     * Compressed a long length of bytes. This includes the bits 0 and 1 in the flag byte, which indicates that this byte is compressed and is long.
+     * Compressed a long length of bytes. This includes the bits 0 and 1 in the flag byte, which indicates that this byte is compressed
+     * and is long.
      * 
-     * @param output The output bytes.
      * @param len
      * @param posy
      */
-    private void writeBytesLongCompression(byte[] output, int len, int posy)
+    private void writeBytesLongCompression(int len, int posy)
     {
-        writeBit(output, 0);
-        writeBit(output, 1);
+        writeBit(0);
+        writeBit(1);
 
         posy = (~posy + 1) << 3;
 
@@ -254,30 +256,27 @@ public class PRSCompressor
     }
 
     /**
-     * @param output The output bytes.
-     * @param len
-     * @param posy
+     * Write compressed bytes to the output byte array.
      */
-    private void writeCompressedBytes(byte[] output, int len, int posy)
+    private void writeCompressedBytes()
     {
-        if (posy > 255 || length > 5)
+        if (pos > 255 || currentCompressionLength > 5)
         {
-            writeBytesLongCompression(output, len, posy);
+            writeBytesLongCompression(currentCompressionLength, pos);
         }
         else
         {
-            writeBytesShortCompression(output, len, posy);
+            writeBytesShortCompression(currentCompressionLength, pos);
         }
     }
 
     /**
-     * Writes an uncompressed byte to the output. This includes the single bit 1 in the flag byte, which indicates that this byte is uncompressed.
-     * 
-     * @param output The output bytes.
+     * Writes an uncompressed byte to the output. This includes the single bit 1 in the flag byte,
+     * which indicates that this byte is uncompressed.
      */
-    private void writeUncompressedByte(byte[] output)
+    private void writeUncompressedByte()
     {
-        writeBit(output, 1);
+        writeBit(1);
         output[outputIndex++] = (byte) (input[inputIndex] & 0xFF);
     }
 }
