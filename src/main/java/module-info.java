56ecef4bf9f8a55c1b25d2aae6ebbcab44a6ff55
Nick <NicholasMoser56@gmail.com>
2019-12-20 23:26:53 -0600

    Use Java 11 modularization

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.github.nicholasmoser {
+  exports com.github.nicholasmoser;
+  exports com.github.nicholasmoser.gamecube;
+  exports com.github.nicholasmoser.gnt4;
+  exports com.github.nicholasmoser.utils;
+  
+  opens com.github.nicholasmoser.gnt4 to javafx.fxml;
+  
+  requires com.google.common;
+  requires com.google.protobuf;
+
+  requires java.desktop;
+  requires java.logging;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.graphics;
+  
+  requires org.apache.commons.io;
+}
