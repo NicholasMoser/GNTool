e0e8896b278d0db230a35a6a9546128483a4e364
Nick <NicholasMoser56@gmail.com>
2019-12-28 01:40:35 -0600

    Check for JAVA_HOME in run.bat

--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -1,1 +1,10 @@
-java --module-path=modules --module com.github.nicholasmoser
+@echo off
+IF "%JAVA_HOME%" == "" (
+    echo JAVA_HOME has not been set. Please follow the installation steps on the GNTool guide.
+    echo https://github.com/NicholasMoser/GNTool/blob/master/README.md
+    echo.
+    pause
+    exit(1)
+)
+
+"%JAVA_HOME%/bin/java" --module-path=modules --module com.github.nicholasmoser
