399ed0c92af579866d87a9356dde313112258812
Nick <NicholasMoser56@gmail.com>
2019-12-28 12:09:34 -0600

    Add Seq and Action from Byakugan

---
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/Action.java
@@ -0,0 +1,29 @@
+package com.github.nicholasmoser.gnt4.seq;
+
+/**
+ * An action in a sequence file.
+ */
+public class Action {
+  private int id;
+  
+  private String description;
+  
+  public Action(int id, String description) {
+    this.id = id;
+    this.description = description;
+  }
+
+  /**
+   * @return The id of the action.
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * @return The description of the action.
+   */
+  public String getDescription() {
+    return description;
+  }
+}
