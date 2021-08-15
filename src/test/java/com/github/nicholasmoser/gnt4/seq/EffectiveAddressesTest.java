cc7b2f279fda93b1d31643196a8141a219fb47db
Nicholas Moser <NicholasMoser56@gmail.com>
2021-08-15 01:01:29 -0500

    Rename tests and add documentation for each

--- src/test/java/com/github/nicholasmoser/gnt4/seq/EffectiveAddressesTest.java
+++ src/test/java/com/github/nicholasmoser/gnt4/seq/EffectiveAddressesTest.java
@@ -9,72 +9,17 @@ import org.junit.jupiter.api.Test;
 public class EffectiveAddressesTest {
 
   /**
-   * 04037c00
+   * Tests calling opcode 04020213.
+   *
+   * <ul>
+   *   <li>First effective address is a general purpose register value.</li>
+   *   <li>Second effective address is a general purpose register value.</li>
+   * </ul>
    *
    * @throws Exception If any Exception occurs
    */
   @Test
-  public void test1() throws Exception {
-    byte[] bytes = new byte[]{0x04, 0x03, 0x7c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x10, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 04021a3f
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test2() throws Exception {
-    byte[] bytes = new byte[]{0x04, 0x02, 0x1a, 0x3f, 0x00, 0x00, 0x00, 0x01};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x8, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 09081d3f
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test3() throws Exception {
-    byte[] bytes = new byte[]{0x09, 0x08, 0x1d, 0x3f, 0x00, 0x01, (byte) 0xbf, 0x24};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x8, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 02033f00
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test4() throws Exception {
-    byte[] bytes = new byte[]{0x02, 0x03, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x10, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 04020213
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test5() throws Exception {
+  public void testGprGpr() throws Exception {
     byte[] bytes = new byte[]{0x04, 0x02, 0x02, 0x13};
     ByteStream bs = new ByteStream(bytes);
     EffectiveAddresses ea = EffectiveAddresses.get(bs);
@@ -84,57 +29,17 @@ public class EffectiveAddressesTest {
   }
 
   /**
-   * 09015300
+   * Tests calling opcode 0616157b.
+   *
+   * <ul>
+   *   <li>First effective address is a general purpose register value.</li>
+   *   <li>Second effective address is a global value plus offset.</li>
+   * </ul>
    *
    * @throws Exception If any Exception occurs
    */
   @Test
-  public void test6() throws Exception {
-    byte[] bytes = new byte[]{0x09, 0x01, 0x53, 0x00, 0x00, 0x00, 0x00, 0x68, 0x7c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x22, 0x1c};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x10, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 04022602
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test7() throws Exception {
-    byte[] bytes = new byte[]{0x04, 0x02, 0x26, 0x02};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x4, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 04034400
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test8() throws Exception {
-    byte[] bytes = new byte[]{0x04, 0x03, 0x44, 0x00, 0x00, 0x00, 0x00, 0x24, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x04};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x10, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 0616157b
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test9() throws Exception {
+  public void testGprGlobal() throws Exception {
     byte[] bytes = new byte[]{0x06, 0x16, 0x15, 0x7b, 0x00, 0x00, 0x00, 0x00};
     ByteStream bs = new ByteStream(bytes);
     EffectiveAddresses ea = EffectiveAddresses.get(bs);
@@ -144,27 +49,37 @@ public class EffectiveAddressesTest {
   }
 
   /**
-   * 04116600
+   * Tests calling opcode 04022602.
+   *
+   * <ul>
+   *   <li>First effective address is a seq stored value.</li>
+   *   <li>Second effective address is a general purpose register value.</li>
+   * </ul>
    *
    * @throws Exception If any Exception occurs
    */
   @Test
-  public void test10() throws Exception {
-    byte[] bytes = new byte[]{0x04, 0x11, 0x66, 0x00, 0x00, 0x00, 0x00, 0x18, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x34};
+  public void testSeqGpr() throws Exception {
+    byte[] bytes = new byte[]{0x04, 0x02, 0x26, 0x02};
     ByteStream bs = new ByteStream(bytes);
     EffectiveAddresses ea = EffectiveAddresses.get(bs);
     System.out.println(ea.getDescription());
-    assertEquals(0x10, bs.offset());
+    assertEquals(0x4, bs.offset());
     assertArrayEquals(bytes, ea.getBytes());
   }
 
   /**
-   * 22052620
+   * Tests calling opcode 22052620.
+   *
+   * <ul>
+   *   <li>First effective address is a seq stored value.</li>
+   *   <li>Second effective address is a seq stored value.</li>
+   * </ul>
    *
    * @throws Exception If any Exception occurs
    */
   @Test
-  public void test11() throws Exception {
+  public void testSeqSeq() throws Exception {
     byte[] bytes = new byte[]{0x22, 0x05, 0x26, 0x20};
     ByteStream bs = new ByteStream(bytes);
     EffectiveAddresses ea = EffectiveAddresses.get(bs);
@@ -174,7 +89,168 @@ public class EffectiveAddressesTest {
   }
 
   /**
-   * 09010293
+   * Tests calling opcode 04021a3f.
+   *
+   * <ul>
+   *   <li>First effective address is a seq stored value.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testSeqOpcode_1() throws Exception {
+    byte[] bytes = new byte[]{0x04, 0x02, 0x1a, 0x3f, 0x00, 0x00, 0x00, 0x01};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x8, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 09081d3f.
+   *
+   * <ul>
+   *   <li>First effective address is a seq stored value.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testSeqOpcode_2() throws Exception {
+    byte[] bytes = new byte[]{0x09, 0x08, 0x1d, 0x3f, 0x00, 0x01, (byte) 0xbf, 0x24};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x8, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 02033f00.
+   *
+   *
+   * <ul>
+   *   <li>First effective address is an opcode offset.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testOpcodeOpcode() throws Exception {
+    byte[] bytes = new byte[]{0x02, 0x03, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x10, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 04034400.
+   *
+   * <ul>
+   *   <li>First effective address is a general purpose register value plus offset.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testGprPlusOffsetOpcode() throws Exception {
+    byte[] bytes = new byte[]{0x04, 0x03, 0x44, 0x00, 0x00, 0x00, 0x00, 0x24, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x04};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x10, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 09015300.
+   *
+   * <ul>
+   *   <li>First effective address is a general purpose register value plus offset.</li>
+   *   <li>Second effective address is a global value plus offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testGprPlusOffsetGlobalPlusOffset() throws Exception {
+    byte[] bytes = new byte[]{0x09, 0x01, 0x53, 0x00, 0x00, 0x00, 0x00, 0x68, 0x7c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x22, 0x1c};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x10, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 04116600.
+   *
+   * <ul>
+   *   <li>First effective address is a seq stored value plus offset.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testSeqPlusOffsetOpcode_1() throws Exception {
+    byte[] bytes = new byte[]{0x04, 0x11, 0x66, 0x00, 0x00, 0x00, 0x00, 0x18, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x34};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x10, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 04026600.
+   *
+   * <ul>
+   *   <li>First effective address is a seq stored value plus offset.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testSeqPlusOffsetOpcode_2() throws Exception {
+    byte[] bytes = new byte[]{0x04, 0x02, 0x66, 0x00, 0x00, 0x00, 0x00, 0x5c, 0x3f, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x10, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 04037c00.
+   *
+   * <ul>
+   *   <li>First effective address is a global value plus offset.</li>
+   *   <li>Second effective address is an opcode offset.</li>
+   * </ul>
+   *
+   * @throws Exception If any Exception occurs
+   */
+  @Test
+  public void testGlobalPlusOffsetOpcode() throws Exception {
+    byte[] bytes = new byte[]{0x04, 0x03, 0x7c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00};
+    ByteStream bs = new ByteStream(bytes);
+    EffectiveAddresses ea = EffectiveAddresses.get(bs);
+    System.out.println(ea.getDescription());
+    assertEquals(0x10, bs.offset());
+    assertArrayEquals(bytes, ea.getBytes());
+  }
+
+  /**
+   * Tests calling opcode 09010293.
    *
    * @throws Exception If any Exception occurs
    */
@@ -185,21 +261,6 @@ public class EffectiveAddressesTest {
     EffectiveAddresses ea = EffectiveAddresses.get(bs);
     System.out.println(ea.getDescription());
     assertEquals(0x8, bs.offset());
-    assertArrayEquals(bytes, ea.getBytes());
-  }
-
-  /**
-   * 04026600
-   *
-   * @throws Exception If any Exception occurs
-   */
-  @Test
-  public void test13() throws Exception {
-    byte[] bytes = new byte[]{0x04, 0x02, 0x66, 0x00, 0x00, 0x00, 0x00, 0x5c, 0x3f, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00};
-    ByteStream bs = new ByteStream(bytes);
-    EffectiveAddresses ea = EffectiveAddresses.get(bs);
-    System.out.println(ea.getDescription());
-    assertEquals(0x10, bs.offset());
     assertArrayEquals(bytes, ea.getBytes());
   }
 }
