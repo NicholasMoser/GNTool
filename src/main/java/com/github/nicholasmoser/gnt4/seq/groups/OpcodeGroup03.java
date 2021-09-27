ef031e27d749d1cd8ca3402d2633c925a2a20994
Nicholas Moser <NicholasMoser56@gmail.com>
2021-09-26 21:49:32 -0500

    Add push and pop opcodes

--- src/main/java/com/github/nicholasmoser/gnt4/seq/groups/OpcodeGroup03.java
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/groups/OpcodeGroup03.java
@@ -2,6 +2,8 @@ package com.github.nicholasmoser.gnt4.seq.groups;
 
 import com.github.nicholasmoser.gnt4.seq.EffectiveAddress;
 import com.github.nicholasmoser.gnt4.seq.opcodes.Opcode;
+import com.github.nicholasmoser.gnt4.seq.opcodes.Pop;
+import com.github.nicholasmoser.gnt4.seq.opcodes.Push;
 import com.github.nicholasmoser.gnt4.seq.opcodes.UnknownOpcode;
 import com.github.nicholasmoser.utils.ByteStream;
 import java.io.IOException;
@@ -10,23 +12,23 @@ public class OpcodeGroup03 {
 
   public static Opcode parse(ByteStream bs, byte opcodeByte) throws IOException {
     return switch (opcodeByte) {
-      case 0x03 -> op_0303(bs);
-      case 0x04 -> op_0304(bs);
+      case 0x03 -> push(bs);
+      case 0x04 -> pop(bs);
       default -> throw new IOException(String.format("Unimplemented: %02X", opcodeByte));
     };
   }
 
-  private static Opcode op_0303(ByteStream bs) throws IOException {
+  private static Opcode push(ByteStream bs) throws IOException {
     int offset = bs.offset();
     EffectiveAddress ea = EffectiveAddress.get(bs);
     String info = String.format(" %s", ea.getDescription());
-    return new UnknownOpcode(offset, ea.getBytes(), info);
+    return new Push(offset, ea.getBytes(), info);
   }
 
-  private static Opcode op_0304(ByteStream bs) throws IOException {
+  private static Opcode pop(ByteStream bs) throws IOException {
     int offset = bs.offset();
     EffectiveAddress ea = EffectiveAddress.get(bs);
     String info = String.format(" %s", ea.getDescription());
-    return new UnknownOpcode(offset, ea.getBytes(), info);
+    return new Pop(offset, ea.getBytes(), info);
   }
 }
