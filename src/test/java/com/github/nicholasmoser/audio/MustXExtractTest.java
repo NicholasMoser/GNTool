664e270b2b042bd6f6f8164d943208ea9af04b2f
Nicholas Moser <NicholasMoser56@gmail.com>
2021-11-01 10:03:18 -0500

    Fix JUnit param module issue, parameterize MusyXExtract tests

--- src/test/java/com/github/nicholasmoser/audio/MustXExtractTest.java
+++ src/test/java/com/github/nicholasmoser/audio/MustXExtractTest.java
@@ -1,8 +1,8 @@
 package com.github.nicholasmoser.audio;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import com.github.nicholasmoser.testing.Prereqs;
 import com.github.nicholasmoser.utils.FileUtils;
@@ -12,17 +12,66 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * Tests the ported functionality of MusyXExtract.
+ */
 public class MustXExtractTest {
-  @Test
-  public void testExtractAndRepack() throws Exception {
-    // Inputs
-    Path uncompressed = Prereqs.getUncompressedGNT4();
-    Path inputSdi = uncompressed.resolve("files/chr/ank/3000.sdi");
-    Path inputSam = uncompressed.resolve("files/chr/ank/3000.sam");
 
+  /**
+   * Streams each .sam and .sdi file from GNT4 as arguments.
+   *
+   * @return Each .sam and .sdi file from GNT4 as arguments.
+   * @throws IOException If GNT4 cannot be parsed.
+   */
+  public static Stream<Arguments> samSdiPathProvider() throws IOException {
+    Path uncompressed = Prereqs.getUncompressedGNT4();
+    List<Path> samFiles = Files.walk(uncompressed).filter(MustXExtractTest::isSamWithSdi)
+        .collect(Collectors.toList());
+    List<Arguments> args = new ArrayList<>();
+    for (Path samFile : samFiles) {
+      Path sdiFile = Paths.get(samFile.toString().replace(".sam", ".sdi"));
+      args.add(arguments(sdiFile, samFile));
+    }
+    return args.stream();
+  }
+
+  /**
+   * If the given file is a .sam file with a correlated .sdi file.
+   *
+   * @param path The file to check if it is a .sam file.
+   * @return If the given file is a .sam file with a correlated .sdi file.
+   */
+  private static boolean isSamWithSdi(Path path) {
+    if (path.toString().endsWith(".sam")) {
+      Path sdi = Paths.get(path.toString().replace(".sam", ".sdi"));
+      if (!Files.exists(sdi)) {
+        throw new IllegalStateException("Found .sam without .sdi: " + path);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Test extracting and repacking each .sam and .sdi file in GNT4.
+   *
+   * @param inputSdi The input .sdi file.
+   * @param inputSam The input .sam file.
+   * @throws Exception If any exception occurs.
+   */
+  @ParameterizedTest(name="#{index} - Test with Argument={0},{1}")
+  @MethodSource("samSdiPathProvider")
+  public void testExtractAndRepack(Path inputSdi, Path inputSam) throws Exception {
     // Outputs
     Path tempDir = FileUtils.getTempDirectory();
     Path outputDir = tempDir.resolve(UUID.randomUUID().toString());
