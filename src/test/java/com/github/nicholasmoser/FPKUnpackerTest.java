87808ead52179ded2e33aad52153f0a3e641a339
Nicholas Moser <NicholasMoser56@gmail.com>
2022-03-08 23:46:08 -0600

    Add note on poor fastcopy performance and why

--- src/test/java/com/github/nicholasmoser/FPKUnpackerTest.java
+++ src/test/java/com/github/nicholasmoser/FPKUnpackerTest.java
@@ -15,6 +15,11 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 public class FPKUnpackerTest {
+  // Performance of fastcopy branch seems to be negligible. In one case it was 1 second faster,
+  // in other cases it was 3 seconds slower. In each case I restarted my PC to avoid disk caching.
+  // Without the fastcopy branch the full copy + unpack process takes around 24 seconds on my HDD.
+  // This does make sense, considering that the FPK files are only ~10% of the full bytes of the
+  // ISO, so this only helps for 10% of the total copying time (17 seconds).
   @Test
   public void testUnpackFPK() throws Exception {
     Path compressedDir = Prereqs.getCompressedGNT4();
