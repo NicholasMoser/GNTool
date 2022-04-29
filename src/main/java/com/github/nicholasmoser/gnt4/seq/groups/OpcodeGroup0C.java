ebaf36c957c30ead9980ea416681ae1748fc2200
Nicholas Moser <NicholasMoser56@gmail.com>
2022-04-28 20:41:05 -0500

    Update opcode group 0C with new mnemonics

--- src/main/java/com/github/nicholasmoser/gnt4/seq/groups/OpcodeGroup0C.java
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/groups/OpcodeGroup0C.java
@@ -1,6 +1,7 @@
 package com.github.nicholasmoser.gnt4.seq.groups;
 
 import com.github.nicholasmoser.gnt4.seq.SEQ_RegCMD2;
+import com.github.nicholasmoser.gnt4.seq.opcodes.MatrixScale;
 import com.github.nicholasmoser.gnt4.seq.opcodes.Opcode;
 import com.github.nicholasmoser.gnt4.seq.opcodes.UnknownOpcode;
 import com.github.nicholasmoser.utils.ByteStream;
@@ -10,15 +11,15 @@ public class OpcodeGroup0C {
 
   public static Opcode parse(ByteStream bs, byte opcodeByte) throws IOException {
     return switch (opcodeByte) {
-      case 0x09 -> op_0C09(bs);
+      case 0x09 -> mtx_scale(bs);
       default -> throw new IOException(String.format("Unimplemented: %02X", opcodeByte));
     };
   }
 
-  private static Opcode op_0C09(ByteStream bs) throws IOException {
+  private static Opcode mtx_scale(ByteStream bs) throws IOException {
     int offset = bs.offset();
     SEQ_RegCMD2 ea = SEQ_RegCMD2.get(bs);
     String info = String.format(" %s", ea.getDescription());
-    return new UnknownOpcode(offset, ea.getBytes(), info);
+    return new MatrixScale(offset, ea.getBytes(), info);
   }
 }
