2bfc6959013363e6e0a978a09474fe5f4f1f3b02
Nicholas Moser <NicholasMoser56@gmail.com>
2022-04-28 20:40:08 -0500

    Update opcode group 0B with new mnemonics

---
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/FloatVectorMove.java
@@ -0,0 +1,43 @@
+package com.github.nicholasmoser.gnt4.seq.opcodes;
+
+import static j2html.TagCreator.attrs;
+import static j2html.TagCreator.div;
+
+import j2html.tags.ContainerTag;
+
+public class FloatVectorMove implements Opcode {
+
+  private final static String MNEMONIC = "fvec_mov";
+  private final int offset;
+  private final byte[] bytes;
+  private final String info;
+
+  public FloatVectorMove(int offset, byte[] bytes, String info) {
+    this.offset = offset;
+    this.bytes = bytes;
+    this.info = info;
+  }
+
+  @Override
+  public int getOffset() {
+    return offset;
+  }
+
+  @Override
+  public byte[] getBytes() {
+    return bytes;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%05X | %s %s %s", offset, MNEMONIC, info, formatRawBytes(bytes));
+  }
+
+  @Override
+  public ContainerTag toHTML() {
+    String id = String.format("#%X", offset);
+    return div(attrs(id))
+        .withText(String.format("%05X | %s %s ", offset, MNEMONIC, info))
+        .with(formatRawBytesHTML(bytes));
+  }
+}
