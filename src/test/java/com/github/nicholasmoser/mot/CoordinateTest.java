82aa1b1d984f4154306af8934e7e96295dc9615b
Nicholas Moser <NicholasMoser56@gmail.com>
2022-05-25 18:18:37 -0500

    Use floats in Coordinate class to maintain precision

--- src/test/java/com/github/nicholasmoser/mot/CoordinateTest.java
+++ src/test/java/com/github/nicholasmoser/mot/CoordinateTest.java
@@ -13,12 +13,10 @@ public class CoordinateTest {
     short short2 = (short) 0x4048; // 3.125
     short short3 = (short) 0x3C93; // 0.017944336
     short short4 = (short) 0x404E; // 3.21875
-    short short5 = (short) 0x0000; // 0.0
     float float1 = (float) -0.0; // 0x8000
     float float2 = (float) 3.125; // 0x4048
     float float3 = (float) 0.017944336; // 0x3C93
     float float4 = (float) 3.21875; // 0x404E
-    float float5 = (float) 0.0; // 0x0000
 
     // Assert that float values can be returned
     Coordinate coordinate = new Coordinate(short1, short2, short3, short4);
@@ -31,34 +29,20 @@ public class CoordinateTest {
     Coordinate duplicateCoordinate = new Coordinate(short1, short2, short3, short4);
     assertEquals(coordinate, duplicateCoordinate);
 
-    // Assert that values can be changed and result in different coordinates
-    coordinate.setFloatX(float5);
-    assertNotEquals(coordinate, duplicateCoordinate);
-    assertEquals(short5, coordinate.getX());
-    assertEquals(float5, coordinate.getFloatX());
-    coordinate.setFloatY(float4);
-    assertEquals(short4, coordinate.getY());
-    assertEquals(float4, coordinate.getFloatY());
-
-    // Validate that truncating floats to a short results in precision loss
-    float pi = (float) 3.1415927;
-    coordinate.setFloatW(pi); // truncated to ~3.140625
-    assertNotEquals(pi, coordinate.getFloatW(), 0.0001); // Too much precision is lost so fail
-    assertEquals(pi, coordinate.getFloatW(), 0.001);
-
     // Test the alternate constructor
     Coordinate otherCoordinate = new Coordinate(float1, float2, float3, float4);
     assertEquals(otherCoordinate, duplicateCoordinate);
 
-    // Validate that the alternate constructor also results in precision loss
+    // Validate that the float constructor results in no precision loss
+    float pi = (float) 3.1415927;
     Coordinate otherCoordinate2 = new Coordinate(pi, pi, pi, pi);
-    assertNotEquals(pi, otherCoordinate2.getFloatX(), 0.0001); // Too much precision is lost so fail
+    assertEquals(pi, otherCoordinate2.getFloatX(), 0.0001);
     assertEquals(pi, otherCoordinate2.getFloatX(), 0.001);
-    assertNotEquals(pi, otherCoordinate2.getFloatY(), 0.0001); // Too much precision is lost so fail
+    assertEquals(pi, otherCoordinate2.getFloatY(), 0.0001);
     assertEquals(pi, otherCoordinate2.getFloatY(), 0.001);
-    assertNotEquals(pi, otherCoordinate2.getFloatZ(), 0.0001); // Too much precision is lost so fail
+    assertEquals(pi, otherCoordinate2.getFloatZ(), 0.0001);
     assertEquals(pi, otherCoordinate2.getFloatZ(), 0.001);
-    assertNotEquals(pi, otherCoordinate2.getFloatW(), 0.0001); // Too much precision is lost so fail
+    assertEquals(pi, otherCoordinate2.getFloatW(), 0.0001);
     assertEquals(pi, otherCoordinate2.getFloatW(), 0.001);
   }
 }
