f3b521e7142e66649a6818afe211b39448af240c
Andreas Kinnunen <kinnu@kth.se>
2022-06-11 17:26:43 +0200

    Added tests for branch_tables

--- src/test/java/com/github/nicholasmoser/gnt4/seq/ext/SeqAssemblerTest.java
+++ src/test/java/com/github/nicholasmoser/gnt4/seq/ext/SeqAssemblerTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.github.nicholasmoser.gnt4.seq.SeqHelper;
 import com.github.nicholasmoser.gnt4.seq.opcodes.Opcode;
 import com.github.nicholasmoser.utils.ByteStream;
+
 import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
 
@@ -47,6 +48,32 @@ public class SeqAssemblerTest {
         }
         Pair<String, String> reassembled = SeqEditor.getOpcodesStrings(assembledOpcodes, 0x1000, bytes.length);
         assertEquals(assembly, reassembled.getValue());
+    }
+
+    @Test
+    void testSeqAssemblyBranchTable() throws IOException {
+        String assembly = Files.readString(Path.of("src/test/resources/gnt4/seq/ext/naruto0x41D4.seqa"));
+        Pair<List <Opcode>, Integer> opcodes = SeqAssembler.assembleLines(assembly.split("\n"), null);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        for (Opcode opcode : opcodes.getKey()) {
+            baos.write(opcode.getBytes(0x41D4, opcodes.getValue()));
+        }
+        byte [] bytes = baos.toByteArray();
+        byte [] reference = Files.readAllBytes(Path.of("src/test/resources/gnt4/seq/ext/naruto0x41D4.seq"));
+        assertArrayEquals(bytes, reference);
+    }
+
+    @Test
+    void testSeqDisassemblyBranchTable() throws IOException {
+        String reference = Files.readString(Path.of("src/test/resources/gnt4/seq/ext/naruto0x41D4.seqa"));
+        byte [] assembled = Files.readAllBytes(Path.of("src/test/resources/gnt4/seq/ext/naruto0x41D4.seq"));
+        List<Opcode> assembledOpcodes = new LinkedList<>();
+        ByteStream bs = new ByteStream(assembled);
+        while (bs.bytesAreLeft()) {
+            assembledOpcodes.add(SeqHelper.getSeqOpcode(bs,bs.peekBytes(2)[0],bs.peekBytes(2)[1]));
+        }
+        Pair<String, String> reassembled = SeqEditor.getOpcodesStrings(assembledOpcodes, 0x41D4, assembled.length);
+        assertEquals(reference, reassembled.getValue());
     }
 
 }
