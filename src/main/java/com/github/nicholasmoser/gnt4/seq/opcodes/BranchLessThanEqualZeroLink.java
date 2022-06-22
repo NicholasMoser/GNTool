ddb960ae3aa223a31f14c5acadb3c170b3c03305
Andreas Kinnunen <kinnu@kth.se>
2022-06-22 16:56:43 +0200

    Adding BranchingLinkingOpcode to opcodes that branch and link

--- src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/BranchLessThanEqualZeroLink.java
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/BranchLessThanEqualZeroLink.java
@@ -1,6 +1,6 @@
 package com.github.nicholasmoser.gnt4.seq.opcodes;
 
-public class BranchLessThanEqualZeroLink extends BranchingOpcode {
+public class BranchLessThanEqualZeroLink extends BranchingOpcode implements BranchingLinkingOpcode {
 
   public BranchLessThanEqualZeroLink(int offset, int destination) {
     super("blezal", new byte[] {0x01, 0x42, 0x00, 0x00}, offset, destination);
--- src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/BranchLessThanZeroLink.java
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/BranchLessThanZeroLink.java
@@ -1,6 +1,6 @@
 package com.github.nicholasmoser.gnt4.seq.opcodes;
 
-public class BranchLessThanZeroLink extends BranchingOpcode {
+public class BranchLessThanZeroLink extends BranchingOpcode implements BranchingLinkingOpcode {
 
   public BranchLessThanZeroLink(int offset, int destination) {
     super("bltzal", new byte[] {0x01, 0x41, 0x00, 0x00}, offset, destination);
