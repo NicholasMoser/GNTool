aca39755a6846c1cbf3db315268c1383e3c5a4fc
Nicholas Moser <NicholasMoser56@gmail.com>
2022-08-14 12:45:17 -0500

    Add ppc code to create cmpwi bytes

---
+++ src/main/java/com/github/nicholasmoser/ppc/CompareWithImmediate.java
@@ -0,0 +1,23 @@
+package com.github.nicholasmoser.ppc;
+
+import com.github.nicholasmoser.utils.ByteUtils;
+import com.google.common.primitives.Bytes;
+
+public class CompareWithImmediate {
+
+  public static byte[] getBytes(int register, int immediate) {
+    if (immediate > 0x7FFF) {
+      throw new IllegalArgumentException("Immediate cannot go over 0x7FFC: " + immediate);
+    } else if (immediate < -0x8000) {
+      throw new IllegalArgumentException("Immediate cannot go under -0x8000: " + immediate);
+    } else if (register < 0) {
+      throw new IllegalArgumentException("Register cannot go under 0: " + register);
+    } else if (register > 31) {
+      throw new IllegalArgumentException("Register cannot go over 31: " + register);
+    }
+    byte[] bytes = new byte[2];
+    bytes[0] = (byte) 0x2C;
+    bytes[1] = (byte) register;
+    return Bytes.concat(bytes, ByteUtils.fromUint16(immediate));
+  }
+}
