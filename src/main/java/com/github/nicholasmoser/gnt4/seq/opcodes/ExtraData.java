3ea7637eaf0f93e64a90437f2ab73ea8e9537364
Nicholas Moser <NicholasMoser56@gmail.com>
2023-07-31 14:33:25 -0500

    String fixes to parse Itachi in SCON4 (#157)

--- src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/ExtraData.java
+++ src/main/java/com/github/nicholasmoser/gnt4/seq/opcodes/ExtraData.java
@@ -26,51 +26,79 @@ public class ExtraData implements Opcode{
 
     public ExtraData(ByteStream bs) throws IOException {
         this.offset = bs.offset();
+        boolean endsEarly = endsEarly(bs);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        baos.write(bs.peekBytes(12));
+        baos.write(bs.peekBytes(endsEarly ? 10 : 12));
         first = bs.readShort();
         second = bs.readShort();
         type = bs.readShort();
         fourth = bs.readShort();
         fifth = bs.readShort();
-        sixth = bs.readShort();
-        switch (type) {
-            case 1 -> {
-                baos.write(bs.readBytes(12));
-                while (!Arrays.equals(bs.peekBytes(2), new byte[]{0x00, 0x00})) {
-                    baos.write(bs.readBytes(10));
+        if (endsEarly) {
+            sixth = 0;
+        } else {
+            sixth = bs.readShort();
+            switch (type) {
+                case 1 -> {
+                    baos.write(bs.readNBytes(12));
+                    while (!Arrays.equals(bs.peekBytes(2), new byte[]{0x00, 0x00})) {
+                        baos.write(bs.readNBytes(10));
+                    }
+                    baos.write(bs.readNBytes(2));
                 }
-                baos.write(bs.readBytes(2));
-            }
-            case 2 -> {
-                inputType = fourth;
-                input = fifth;
-                if (bs.peekWord() == 0) {
-                    baos.write(bs.readBytes(4));
-                } else {
-                    if (inputType == 0x2270) {
-                        baos.write(bs.readBytes(14));
-                    } else if (inputType == 0x227F) {
-                        baos.write(bs.readBytes(22));
-                        while (!Arrays.equals(bs.peekBytes(2), new byte[]{0x00, 0x00})) {
-                            baos.write(bs.readBytes(10));
+                case 2 -> {
+                    inputType = fourth;
+                    input = fifth;
+                    if (bs.peekWord() == 0) {
+                        baos.write(bs.readNBytes(4));
+                    } else {
+                        if (inputType == 0x2270) {
+                            baos.write(bs.readNBytes(14));
+                        } else if (inputType == 0x227F) {
+                            baos.write(bs.readNBytes(22));
+                            while (!Arrays.equals(bs.peekBytes(2), new byte[]{0x00, 0x00})) {
+                                baos.write(bs.readNBytes(10));
+                            }
+                            baos.write(bs.readNBytes(2));
+                        } else if (inputType == 0x4000) {
+                            boolean isSCON4Itachi = false;
+                            while (!Arrays.equals(bs.peekBytes(8), new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})) {
+                                baos.write(bs.readNBytes(4));
+                                // Workaround for Itachi in SCON4, who has String Active Strings
+                                // begin 4 bytes earlier than expected
+                                if (Arrays.equals(bs.peekBytes(0xE), new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x19, (byte) 0x88, 0x00, 0x02, (byte) 0x92, 0x58})) {
+                                    isSCON4Itachi = true;
+                                    break;
+                                }
+                            }
+                            baos.write(bs.readNBytes(isSCON4Itachi ? 6 : 8));
                         }
-                        baos.write(bs.readBytes(2));
-                    } else if (inputType == 0x4000) {
-                        while (!Arrays.equals(bs.peekBytes(8), new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})) {
-                            baos.write(bs.readBytes(4));
-                        }
-                        baos.write(bs.readBytes(8));
                     }
                 }
-            }
-            case 3 -> {
-                inputType = fourth;
-                input = fifth;
-                baos.write(bs.readBytes(4));
+                case 3 -> {
+                    inputType = fourth;
+                    input = fifth;
+                    baos.write(bs.readNBytes(4));
+                }
             }
         }
         this.bytes = baos.toByteArray();
+    }
+
+    /**
+     * In SCON4, some ExtraData end early at 10 bytes instead of 12 bytes. This method checks for
+     * that by looking at where the next ExtraData starts and ends.
+     *
+     * @param bs The ByteStream.
+     * @return If the ExtraData ends early.
+     * @throws IOException If any I/O exception occurs.
+     */
+    private boolean endsEarly(ByteStream bs) throws IOException {
+        int offset = bs.offset();
+        bs.skip(10);
+        boolean endsEarly = bs.readWord() == 0x1000A;
+        bs.seek(offset);
+        return endsEarly;
     }
 
     @Override
